c2a705c42a875931c4e6071056b580906858e348
Andy Powers <apowers@ncsu.edu>
2024-10-28 21:10:58 -0400

    started app

--- app.R
+++ app.R
@@ -1,50 +1,67 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+# This app will enable exploration of data representing mobile device attributes and behaviors.
+# For further references, see associated [Quarto file](static-app-prototype.qmd).
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+# UI definition
+### somewhere must use dynamic text
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    # Application title panel
+    titlePanel("Project 2: Exploring Mobile Device Data"),
 
-    # Sidebar with a slider input for number of bins 
+    #author
+    h3("Andy Powers"),
+    h4(
+      tags$a(
+        href="mailto:apowers@ncsu.edu",
+        "apowers@ncsu.edu")
+      ),
+
+    # sidebar with widgets for subsetting
+    ### 2+ categorical vars: offer select levels as well as 'all'
+    ### 2+ numeric variable: dynamic method for slider 
+    ### action button to subset per selections (vs auto-sub as edited)
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            #inputs here
         ),
 
-        # Show a plot of the generated distribution
+        # main panel with tabs
+        
+        ### About tab, default start
+        ##### Purpose of app
+        ##### Data, source, link
+        ##### tell purpose of sidebar and each tab
+        ##### relevant picture
+        
+        ### Data Download tab
+        ##### display with dataTableOutput / renderDataTable
+        ##### subset per selections/action button
+        ##### save data via downloadButton
+        
+        ### Data Exploration tab
+        ##### obtain the num/graph summaries from the prototype build contents
+        ##### design as I like - subtabs, dynamic UI, etc.
+        ##### subset when selected/action
+        ##### user can display cat or num var summaries
+        ##### user selects any cat or num vars to summarize, plus any modifying the summary
+        ####### for ex: select num var for num summary, plus cat var to summarize across
+        ####### for ex: plots, select which var on x or y, coloring, etc.
+        ##### account for error messages, plus loading spinners for waits
         mainPanel(
-           plotOutput("distPlot")
+           #plots etc.
         )
     )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+# server definition
+### somewhere must use dynamic text
+server <- function(input, output, session) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+    #output$distPlot <- renderPlot({
+        
+    #})
 }
 
 # Run the application 
